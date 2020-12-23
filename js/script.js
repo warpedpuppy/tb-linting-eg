e465a21f45707ad27541d9b8054fa0103590e6ca
Ty4jbt <tyaboe@gmail.com>
2020-12-23 14:25:36 -0800

    added objects to an array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,65 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = "Filet Mignon";
-document.write(favoriteFood)
+let pokemon001 = {
+    name: 'Balbasaur',
+    height: 2.3,
+    type: ['grass', 'poison'],
+};
+
+let pokemon002 = {
+    name: 'Ivysaur',
+    height: 3.25,
+    type: ['grass', 'poison'],
+};
+
+let pokemon003 = {
+    name: 'Venusaur',
+    height: 6.58,
+    type: ['grass', 'poison'],
+};
+
+let pokemon004 = {
+    name: 'Charmander',
+    height: 2,
+    type: ['fire'],
+};
+
+let pokemon005 = {
+    name: 'Charmeleon',
+    height: 3.58,
+    type: ['fire'],
+};
+
+let pokemon006 = {
+    name: 'Charizard',
+    height: 5.58,
+    type: ['fire', 'flying'],
+};
+
+let pokemon007 = {
+    name: 'Squirtle',
+    height: 1.67,
+    type: ['water'],
+};
+
+let pokemon008 = {
+    name: 'Wartortle',
+    height: 3.25,
+    type: ['water'],
+};
+
+let pokemon009 = {
+    name: 'Blastoise',
+    height: 5.25,
+    type: ['water'],
+};
+
+pokemonList.push(pokemon001);
+pokemonList.push(pokemon002);
+pokemonList.push(pokemon003);
+pokemonList.push(pokemon004);
+pokemonList.push(pokemon005);
+pokemonList.push(pokemon006);
+pokemonList.push(pokemon007);
+pokemonList.push(pokemon008);
+pokemonList.push(pokemon009);
