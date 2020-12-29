f7bc615c7fb5b5fd88498f715cc006c18739d249
Ty4jbt <tyaboe@gmail.com>
2020-12-29 11:49:04 -0800

    tidied up unnecessary JavaScript to make code cleaner

--- js/script.js
+++ js/script.js
@@ -1,77 +1,26 @@
 // Array name
-let pokemonList = [];
+let pokemonList = [
+    // Pokemon listed as objects
+    { name: 'Balbasaur', height: 2.3, type: ['grass', 'poison'] },
+    { name: 'Ivysaur', height: 3.25, type: ['grass', 'poison'] },
+    { name: 'Venusaur', height: 6.58, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: 2, type: ['fire'] },
+    { name: 'Charmeleon', height: 3.58, type: ['fire'] },
+    { name: 'Charizard', height: 5.58, type: ['fire', 'flying'] },
+    { name: 'Squirtle', height: 1.67, type: ['water'] },
+    { name: 'Wartortle', height: 3.25, type: ['water'] },
+    { name: 'Blastoise', height: 5.25, type: ['water'] },
+];
 
-// Pokemon listed as objects
-let pokemon001 = {
-    name: 'Balbasaur',
-    height: 2.3,
-    type: ['grass', 'poison'],
-};
-
-let pokemon002 = {
-    name: 'Ivysaur',
-    height: 3.25,
-    type: ['grass', 'poison'],
-};
-
-let pokemon003 = {
-    name: 'Venusaur',
-    height: 6.58,
-    type: ['grass', 'poison'],
-};
-
-let pokemon004 = {
-    name: 'Charmander',
-    height: 2,
-    type: ['fire'],
-};
-
-let pokemon005 = {
-    name: 'Charmeleon',
-    height: 3.58,
-    type: ['fire'],
-};
-
-let pokemon006 = {
-    name: 'Charizard',
-    height: 5.58,
-    type: ['fire', 'flying'],
-};
-
-let pokemon007 = {
-    name: 'Squirtle',
-    height: 1.67,
-    type: ['water'],
-};
-
-let pokemon008 = {
-    name: 'Wartortle',
-    height: 3.25,
-    type: ['water'],
-};
-
-let pokemon009 = {
-    name: 'Blastoise',
-    height: 5.25,
-    type: ['water'],
-};
-
-// To push each object into the array
-pokemonList.push(pokemon001);
-pokemonList.push(pokemon002);
-pokemonList.push(pokemon003);
-pokemonList.push(pokemon004);
-pokemonList.push(pokemon005);
-pokemonList.push(pokemon006);
-pokemonList.push(pokemon007);
-pokemonList.push(pokemon008);
-pokemonList.push(pokemon009);
-
-// To list out the Name and Height of each Pokemon
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`${pokemonList[i].name} `);
-    document.write(`(${pokemonList[i].height}) `);
-    if (pokemonList[i].height > 5) {
-        document.write("Wow that's tall! ");
+//creating a function to repeat the for loop when called upon
+function printingArrayDetails(list) {
+    // To list out the Name and Height of each Pokemon
+    for (let i = 0; i < list.length; i++) {
+        document.write(`<p> ${pokemonList[i].name}, ${pokemonList[i].height} </p>`);
+        if (pokemonList[i].height > 5) {
+        document.write("<p>" + "Wow that's big!" + "</p>");
+        };
     };
 };
+
+printingArrayDetails(pokemonList);
